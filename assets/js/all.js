191e23320942411bc2cc16de7b85ffe75ad9c26f
natsu19 <crookedjoker000@gmail.com>
2021-09-01 15:45:05 +0800

    Update 2021-09-01T07:45:03.211Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,54 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  //Aos
+  AOS.init({
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120,
+    // offset (in px) from the original trigger point
+    delay: 0,
+    // values from 0 to 3000, with step 50ms
+    duration: 500,
+    // values from 0 to 3000, with step 50ms
+    easing: 'ease',
+    // default easing for AOS animations
+    once: false,
+    // whether animation should happen only once - while scrolling down
+    mirror: false,
+    // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
+
+  }); // console.log('Hello!');
+  // $(document).ready(() => {
+  //   console.log('HesSchool Hello!');
+  // });
+  //swiper
+  //首頁課程介紹
+  //courseSwiper
+
+  var autoPlaySwiper = document.querySelector('.autoPlaySwiper');
+
+  if (autoPlaySwiper) {
+    var swiper = new Swiper(".autoPlaySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      autoplay: {
+        delay: 2000
+      },
+      grid: {
+        rows: 2 //fill: 'row',
+
+      },
+      breakpoints: {
+        992: {
+          // loop: true,
+          slidesPerView: 1.5,
+          grid: {
+            rows: 1
+          }
+        }
+      }
+    });
+  }
 });
 //# sourceMappingURL=all.js.map

fe727ba514c082596ae8f11410090e851df0fa20
natsu19 <crookedjoker000@gmail.com>
2021-09-04 03:08:38 +0800

    Update 2021-09-03T19:08:35.961Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,7 @@
 "use strict";
 
 $(document).ready(function () {
+  //--- start ---
   //Aos
   AOS.init({
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
@@ -18,7 +19,7 @@ $(document).ready(function () {
     // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
 
-  }); // console.log('Hello!');
+  }); // console.log('Hello World!');
   // $(document).ready(() => {
   //   console.log('HesSchool Hello!');
   // });
@@ -32,6 +33,7 @@ $(document).ready(function () {
     var swiper = new Swiper(".autoPlaySwiper", {
       slidesPerView: 2,
       spaceBetween: 30,
+      loop: true,
       autoplay: {
         delay: 2000
       },
@@ -90,6 +92,83 @@ $(document).ready(function () {
 
       }
     });
-  }
+  } //立即預約-選擇等級
+  // const ClassLevel = document.querySelector('.ClassLevel');
+  // if (ClassLevel) {
+  // const swiper = new Swiper(".ClassLevel", {
+  //   slidesPerView: 1,
+  //   spaceBetween: 30,
+  //   grid: {
+  //     fill: 'row',
+  //   },
+  //   breakpoints: {
+  //     768:{
+  //       slidesPerView: 1.5,
+  //       rows: 1,
+  //       // loop: true,
+  //     },
+  //     992:{
+  //       slidesPerView: 2.3,
+  //       rows: 1,
+  //     },
+  //     1200:{
+  //       slidesPerView: 3,
+  //       spaceBetween: 20,
+  //       rows: 1,
+  //     },
+  //   }
+  // });
+  // }
+
+
+  var ClassLevel = new Swiper(".ClassLevel", {
+    slidesPerView: 1,
+    slidesPerColumn: 3,
+    spaceBetween: 24,
+    slidesPerColumnFill: 'row',
+    breakpoints: {
+      768: {
+        slidesPerView: 1.8,
+        slidesPerColumn: 1,
+        spaceBetween: 30
+      },
+      960: {
+        slidesPerView: 3,
+        slidesPerColumn: 1,
+        spaceBetween: 20
+      }
+    }
+  }); //首次體驗 
+
+  $('.FirstBtn').click(function () {
+    //把其他兩個卡片藏起來
+    $('.pills-ShortTerm,.pills-LongTerm').toggleClass('d-none d-lg-block');
+  }); //點填寫資料、完成預約等分頁，"選擇課程階級"會隱藏
+
+  $('.projectPage').click(function () {
+    $('.reserveRemind').slideDown();
+  });
+  $('.fillDataPage,.reservePage').click(function () {
+    $('.reserveRemind,.courseList').slideUp();
+  }); // const ClassLevel = document.querySelector('.ClassLevel');
+  // if (ClassLevel) {
+  // const  swiper = new Swiper(".ClassLevel", {
+  //     slidesPerView: 2,
+  //     spaceBetween: 30,
+  //     grid: {
+  //       rows: 1,    
+  //       fill: 'row',
+  //     },
+  //     breakpoints: {
+  //       992:{
+  //         slidesPerView: 3,
+  //         grid: {
+  //           rows: 1, 
+  //         },
+  //       },
+  //     }
+  //   });
+  // }
+  //--- end ---
 });
 //# sourceMappingURL=all.js.map
